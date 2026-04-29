740e8fdf0273f4120f3d68ee7e61e0b4008bad8f
Karthikeyan Vaiyapuri <karthikasturi@gmail.com>
2026-04-29 22:03:55 +0530

    Updated few more code changes.

--- app/db/repositories/tags.py
+++ app/db/repositories/tags.py
@@ -6,7 +6,9 @@ from app.db.repositories.base import BaseRepository
 
 class TagsRepository(BaseRepository):
     async def get_all_tags(self) -> List[str]:
-        tags_row = await queries.get_all_tags(self.connection)
+        tags_row = []
+        async for row in queries.get_all_tags(self.connection):
+            tags_row.append(row)
         return [tag[0] for tag in tags_row]
 
     async def create_tags_that_dont_exist(self, *, tags: Sequence[str]) -> None:
